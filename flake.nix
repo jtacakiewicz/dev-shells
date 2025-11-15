2749d23cd0904d35331ec31e7c5115fc283a89be
Jan Tacakiewicz <jan.tacakiewicz@protonmail.com>
2025-11-15 14:10:25 +0100

    flake: making shells for all used systems

--- flake.nix
+++ flake.nix
@@ -1,23 +1,36 @@
 {
-    description = "A very basic flake";
+    description = "Development shells";
 
     inputs = {
         nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     };
 
-    outputs = { self, nixpkgs, ... }: 
+    outputs = { self, nixpkgs, ... }:
         let
-            system = "x86_64-linux";
-            pkgs = import nixpkgs { inherit system; };
+            # Define all systems you want to support
+            systems = [
+                "x86_64-linux"
+                "aarch64-linux"
+                "x86_64-darwin"
+                "aarch64-darwin"
+            ];
+
+            forAllSystems = f: nixpkgs.lib.genAttrs systems (system:
+                let
+                    pkgs = import nixpkgs { inherit system; };
+                in
+                    f pkgs
+            );
         in
             {
-            devShells.${system} = {
-                python = pkgs.callPackage ./shells/python.nix { };
-                vulkan = pkgs.callPackage ./shells/vulkan.nix { };
-                react = pkgs.callPackage ./shells/react.nix { };
-                bigdata = pkgs.callPackage ./shells/bigdata.nix { };
-                android = pkgs.callPackage ./shells/android.nix { };
-                gostack = pkgs.callPackage ./shells/gofullstack.nix { };
-            };
+            devShells = forAllSystems (pkgs: {
+                python   = pkgs.callPackage ./shells/python.nix { };
+                vulkan   = pkgs.callPackage ./shells/vulkan.nix { };
+                react    = pkgs.callPackage ./shells/react.nix { };
+                bigdata  = pkgs.callPackage ./shells/bigdata.nix { };
+                android  = pkgs.callPackage ./shells/android.nix { };
+                gostack  = pkgs.callPackage ./shells/gofullstack.nix { };
+            });
         };
 }
+
